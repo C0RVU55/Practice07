cfd4aeb1a33b1b9e947f845d28e68b7e7565a05f
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-03 16:21:21 +0900

    abstract-class_interface

--- src/com/javaex/ex01/Ractangle.java
+++ src/com/javaex/ex01/Ractangle.java
@@ -11,7 +11,25 @@ public class Ractangle extends Shape{
 		this.height=height;
 	}
 	
-	public void showInfo(){
+	//ShapeApp에서 가로 길이를 출력하기 위해 메소드 getter/setter 추가
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	//Shape에 draw 추상메소드가 있기 때문에 자식도 draw메소드를 갖고 있어야 됨.
+	public void draw(){
 		System.out.println("[사각형]#면색:" +super.fillColor+ " "
 				+ " #가로:" + width 
 				+" #세로:" + height + "그렸습니다.");                                                                        
--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,15 +4,15 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-		Shape s = new Shape("빨강");
+		//Shape s = new Shape("빨강"); --> Shape는 추상클래스라 객체화(new) 불가능.
 		Circle c1 = new Circle("녹색", 10);
 		Ractangle r1 = new Ractangle("빨강", 4, 4);
-		Shape sr1 = new Ractangle("빨강", 4, 4);
+		Shape sr1 = new Ractangle("빨강", 12, 10); //문제에 맞게 값 수정
 		
 		//sr1을 그리는 메소드 호출
-		
-		//sr1의 가로크기 출력
-		
+		sr1.draw();
+		//sr1의 가로크기 출력 --> Ractangle에 메소드 getter 추가하고 sr1 다운캐스팅해서 자식클래스의 메소드 호출.
+		System.out.println(((Ractangle)sr1).getWidth());
 	}
 }
 
