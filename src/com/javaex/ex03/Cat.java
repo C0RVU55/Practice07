1e68442d70ef2add0921feea7d5008ecb0c8c947
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-03 17:15:32 +0900

    abstract-class_interface

--- src/com/javaex/ex03/Cat.java
+++ src/com/javaex/ex03/Cat.java
@@ -2,5 +2,8 @@ package com.javaex.ex03;
 
 public class Cat implements Soundable {
 
-
+	//인터페이스에 있는 메소드의 내용을 채움.
+	public String sound() {
+		return "야옹";
+	}
 }
--- src/com/javaex/ex03/SoundApp.java
+++ src/com/javaex/ex03/SoundApp.java
@@ -10,7 +10,8 @@ public class SoundApp {
     }
 
     public static void printSound( Soundable soundable ) {
-        //구현
+        //구현 --> soundable에 대입된 클래스(Soundable soundable=new Cat();)에서 sound 메소드 호출 후 출력.
+    	System.out.println(soundable.sound());
     }
     
 }
